472de9bd49fbddd84f34ebbb0e85aa9b6493d8b7
Dana Ng <ngdana1@gmail.com>
2018-11-03 22:31:07 -0400

    Setup components for chat console

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Grid } from 'semantic-ui-react';
 import './App.css';
+import ColorPanel from './ColorPanel';
+import SidePanel from './SidePanel';
+import Messages from './Messages';
+import MetaPanel from './MetaPanel';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>App works</h1>
-      </div>
-    );
-  }
-}
+
+const App = () => (
+  <Grid columns="equal" className="app" style={{background: "#eee"}}>
+    <ColorPanel />
+    <SidePanel />
+    <Grid.Column>
+      <Messages />
+    </Grid.Column>
+    <Grid.Column>
+      <MetaPanel />
+    </Grid.Column>
+  </Grid>
+)
 
 export default App;
