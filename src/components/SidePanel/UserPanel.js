8822282633db9b0b2157ec239aa0c9e03f0c7113
Dana Ng <ngdana1@gmail.com>
2018-11-04 09:23:39 -0500

    Added style for side panel, added dropdown menu with options

---
+++ src/components/SidePanel/UserPanel.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
+
+export default class UserPanel extends Component {
+  dropdownOptions = () => [
+    {
+      key: 'user',
+      text: <span>Signed in as <strong>User</strong></span>,
+      disbaled: true
+    },
+    {
+      key: 'avatar',
+      text: <span>Change Avatar</span>
+    },
+    {
+      key: 'signout',
+      text: <span>Sign Out</span>
+    }
+  ]
+
+  render() {
+    return (
+      <Grid style={{ background: '#4c3c4c'}}>
+        <Grid.Column>
+          <Grid.Row style={{ padding: '1.2em', margin: 0 }}>
+            {/* App Header */}
+            <Header inverted floated="left" as="h2">
+              <Icon name="code" />
+              <Header.Content>DevChat</Header.Content>
+            </Header>
+          </Grid.Row>
+          {/* User Dropdown */}
+          <Header style={{ padding: '0.25em'}} as="h4" inverted>
+            <Dropdown 
+              trigger={<span>User</span>}
+              options={this.dropdownOptions()}
+            />
+          </Header>
+        </Grid.Column>
+      </Grid>
+    )
+  }
+}
