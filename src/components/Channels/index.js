eaae007600de82319989f4d601d9a473ca675038
Dana Ng <ngdana1@gmail.com>
2018-11-14 10:27:59 -0500

    Added ability to add new channels to firebase

--- src/components/Channels/index.js
+++ src/components/Channels/index.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
+import firebase from '../../firebase';
 import { Menu, Icon, Modal, Form, Input, Button } from 'semantic-ui-react';
 
 export default class Channels extends Component {
   state={
+    user: this.props.currentUser,
     channels: [],
     channelName: '',
     channelDetails: '',
+    channelsRef: firebase.database().ref('channels'),
     isModalOpen: false
   }
+
+  addChannel = () => {
+    const { channelsRef, channelName, channelDetails, user} = this.state;
+    const key = channelsRef.push().key; // unique key
+    const newChannel = {
+      id: key,
+      name: channelName,
+      details: channelDetails,
+      createdBy: {
+        name: user.displayName,
+        avatar: user.photoURL
+      }
+    }
+
+    channelsRef
+      .child(key)
+      .update(newChannel)
+      .then(() => {
+        this.setState({ channelName: '', channelDetails: ''});
+        this.closeModal();
+        console.log('channel added');
+      })
+      .catch(err => console.error(err))
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if(this.isFormValid(this.state)) {
+      this.addChannel()
+    }
+  }
+
+  isFormValid = ({ channelName, channelDetails}) => (
+    channelName && channelDetails
+  )
 
   openModal = () => this.setState({isModalOpen: true});
 
@@ -34,7 +72,7 @@ export default class Channels extends Component {
         <Modal basic open={isModalOpen} onClose={this.closeModal}>
           <Modal.Header>Add Channel</Modal.Header>
           <Modal.Content>
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
               <Form.Field>
                 <Input 
                   fluid
@@ -53,7 +91,7 @@ export default class Channels extends Component {
                 />
               </Form.Field>
               <Modal.Actions>
-                <Button color="green" inverted>
+                <Button color="green" inverted onClick={this.handleSubmit}>
                   <Icon name="checkmark" /> Add
                 </Button>
                 <Button color="red" inverted onClick={this.closeModal}>
