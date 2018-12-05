b02a4e3b840d5215818666e374c73454c95ed759
Dana Ng <ngdana1@gmail.com>
2018-12-04 22:52:40 -0500

    Added new components and styled the form and header

--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -6,8 +6,19 @@ export default class MessagesHeader extends Component {
     return (
       <Segment clearing>
         <Header fluid="true" as="h2" floated="left" style={{ marginBottom: 0 }}>
-        Channel
-        <Icon name="star outline" color="black"/>
+          <span>
+            Channel
+            <Icon name="star outline" color="black"/>
+          </span>
+          <Header.Subheader>2 Users</Header.Subheader>
+        </Header>
+        <Header floated="right">
+          <Input
+            size="mini"
+            icon="search"
+            name="searchTerm"
+            placeholder="Search Messages"
+          />
         </Header>
       </Segment>
     )
