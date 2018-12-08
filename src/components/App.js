b81000697103ade81a9a7aee001ee751440f26b1
Dana Ng <ngdana1@gmail.com>
2018-12-08 07:30:19 -0500

    Added capability for adding messages and error handling

--- src/components/App.js
+++ src/components/App.js
@@ -8,12 +8,19 @@ import Messages from './Messages';
 import MetaPanel from './MetaPanel';
 
 
-const App = ({ currentUser }) => (
+const App = ({ currentUser, currentChannel }) => (
   <Grid columns="equal" className="app" style={{background: "#eee"}}>
     <ColorPanel />
-    <SidePanel currentUser={currentUser}/>
+    <SidePanel 
+      currentUser={currentUser}
+      currentUser={currentUser && currentUser.uid}
+    />
     <Grid.Column style={{ marginLeft: 320 }}>
-      <Messages />
+      <Messages
+        key={currentChannel && currentChannel.id}
+        currentChannel={currentChannel}
+        currentUser={currentUser}
+      />
     </Grid.Column>
     <Grid.Column width={4}>
       <MetaPanel />
@@ -22,7 +29,8 @@ const App = ({ currentUser }) => (
 )
 
 const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+  currentUser: state.user.currentUser,
+  currentChannel: state.channel.currentChannel
 })
 
 export default connect(mapStateToProps)(App);
--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -1,17 +1,77 @@
 import React, { Component } from 'react'
+import firebase from '../../firebase'
 import {Segment, Button, Input} from 'semantic-ui-react'
 
 export default class MessageForm extends Component {
+  state = {
+    message: '',
+    loading: false,
+    channel: this.props.currentChannel,
+    user: this.props.currentUser,
+    errors: []
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  createMessage = () => {
+    const {user, message} = this.state;
+    const newMessage = {
+      timestamp: firebase.database.ServerValue.TIMESTAMP,
+      content: message,
+      user: {
+        id: user.uid,
+        name: user.displayName,
+        avatar: this.state.user.photoURL
+      }
+    }
+    return newMessage
+  }
+
+  sendMessage = () => {
+    const { messagesRef } = this.props;
+    const { message, channel, errors } = this.state;
+
+    if ( message ) {
+      this.setState({ loading: true });
+      messagesRef
+        .child(channel.id)
+        .push() // push onto messagesRef
+        .set(this.createMessage())
+        .then(() => {
+          this.setState({loading: false, message: '', errors: []})
+        })
+        .catch(err => {
+          console.error(err);
+          this.setState({
+            loading: false,
+            errors: errors.concat(err)
+          })
+        })
+    } else {
+      this.setState({
+        errors: this.state.errors.concat({ message: 'Add a message.'})
+      })
+    }
+  }
+
   render() {
+    const { errors } = this.state;
+
     return (
       <Segment className="message__form">
         <Input 
           fluid
           name="message"
+          onChange={this.handleChange}
           style={{marginBottom: '0.7em'}}
           label={<Button icon="add" />}
           labelPosition="left"
           placeholder="Write your message"
+          className={
+            errors.some(error => error.message.includes('message')) ? 'error': ''
+          }
         />
         <Button.Group
           icon
@@ -19,6 +79,7 @@ export default class MessageForm extends Component {
         >
           <Button 
             color="orange"
+            onClick={this.sendMessage}
             content="Add Reply"
             labelPosition="left"
             icon="edit"
